3e148609be3fc1a3367ada0acddc912c0b325fae
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-09 15:21:57 +0600

    chore: Update app.js to use Vue.js and vue-router for improved routing and rendering

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import App from './components/app.vue';
+import router from './router/index.js';
+
+createApp(App).use(router).mount('#app');
