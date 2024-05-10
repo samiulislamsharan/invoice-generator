db758a4aa80a0018c506188c340c02edbae32e14
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-10 14:44:29 +0600

    feat: Update invoices index page to fetch and display all invoices

--- resources/js/components/invoices/index.vue
+++ resources/js/components/invoices/index.vue
@@ -5,10 +5,14 @@ let invoices = ref([])
 let searchInvoice = ref([])
 
 onMounted(async () => {
-    getInvoices();
+    getInvoices()
 });
 
 const getInvoices = async () => {
+    let response = await axios.get("/api/get_all_invoice")
+    console.log('response', response)
+    invoices.value = response.data.invoices
+};
 
 const search = async () => {
     let response = await axios.get('/api/search_invoice?s='+searchInvoice.value)
@@ -79,7 +83,7 @@ const search = async () => {
                     <p>Total</p>
                 </div>
 
-                <!-- item 1 -->
+                <!--==================== RENDER INVOICE LIST ====================-->
                 <div class="table--items" v-for="item in invoices" :key="item.id" v-if="invoices.length > 0">
                     <a href="#" class="table--items--transactionId">#{{item.id}}</a>
                     <p>{{item.date}}</p>
