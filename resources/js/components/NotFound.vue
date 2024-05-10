ab1cabe2ea0ed4af3d111ffba6ce10f3098f37bf
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-10 14:53:48 +0600

    refactor(components): :recycle: update NotFound component to use a container for better styling

--- resources/js/components/NotFound.vue
+++ resources/js/components/NotFound.vue
@@ -1,5 +1,7 @@
 <template>
-    <h1>404 - Not Found</h1>
-    <p>Sorry, the page you are looking for does not exist.</p>
-    <router-link to="/">Go back to Home Page</router-link>
+    <div class="container">
+        <h2 class="invoice__title">404 - Not Found</h2>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <router-link to="/" style="color:teal">Go back to Home Page</router-link>
+    </div>
 </template>
