216acdcf659848e0ff9ed3395a288adad888af35
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-10 14:38:33 +0600

    feat(controllers): :sparkles: add search method in InvoiceController

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -15,4 +15,20 @@ class InvoiceController extends Controller
             'invoices' => $invoices
         ], 200);
     }
-}
+
+    public function search_invoice(Request $request)
+    {
+        $search = $request->get('s');
+
+        if ($search != null) {
+            $invoices = Invoice::with('customer')
+                ->where('id', 'LIKE', "%$search%")
+                ->get();
+
+            return response()->json([
+                'invoices' => $invoices
+            ], 200);
+        } else {
+            return $this->get_all_invoice();
+        }
+    }
