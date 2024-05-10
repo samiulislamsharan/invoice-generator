6c2d8d885de94e8f4b269254fd1ad9a2872c9603
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-10 14:52:25 +0600

    feat(router): :sparkles: add new invoice creation page and route
    The code changes introduce a new Vue component for creating invoices. This commit adds the template and basic structure for the new invoice creation page.

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,12 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router"
 
 import invoiceIndex from '../components/invoices/index.vue';
+import invoiceNew from '../components/invoices/new.vue';
 import notFound from '../components/NotFound.vue';
 
 const routes = [
     {
         path: '/',
         component: invoiceIndex,
+    },
+    {
+        path: '/invoice/new',
+        component: invoiceNew,
     },
     {
         path: '/:pathMatch(.*)*',
