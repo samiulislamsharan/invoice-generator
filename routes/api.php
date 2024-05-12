48542af5538e567ed709b0af7826053148811b28
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-13 01:47:39 +0600

    feat(api-routes): Add endpoint for retrieving all products

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,3 +27,5 @@ Route::get('/create_invoice', [InvoiceController::class, 'create_invoice']);
 Route::post('/add_invoice', [InvoiceController::class, 'add_invoice']);
 
 Route::get('/customers', [CustomerController::class, 'all_customer']);
+
+Route::get('/products', [ProductController::class, 'all_product']);
