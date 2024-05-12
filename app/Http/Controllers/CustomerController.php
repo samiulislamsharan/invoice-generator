c2c2b162e693fe6561c16f3f84f3263ca0f99d9d
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-13 01:45:47 +0600

    feat(controllers): :sparkles: add method to fetch all customers
    This commit introduces a new method `all_customer` in the `CustomerController`:
    1. The `all_customer` method fetches all customers from the database, ordered by their 'id' in descending order.
    2. It then returns a JSON response containing the customers.
    This new feature enhances the application by providing an endpoint to fetch all customer data, which can be used in various parts of the application where customer information is needed.

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public function all_customer()
+    {
+        $customers = Customer::orderBy('id', 'DESC')->get();
+
+        return response()->json([
+            'customers' => $customers,
+        ], 200);
+    }
 }
