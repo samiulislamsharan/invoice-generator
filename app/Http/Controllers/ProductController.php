6b99fb7325b893b58fe9a6835b6d0800079d38a7
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-13 01:47:21 +0600

    feat(controllers): :sparkles: add method to fetch all products
    This commit introduces a new method `all_product` in the `ProductController`:
    1. The `all_product` method fetches all products from the database, ordered by their 'id' in descending order.
    2. It then returns a JSON response containing the products.
    
    This new feature enhances the application by providing an endpoint to fetch all product data, which can be used in various parts of the application where product information is needed.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function all_product()
+    {
+        $products = Product::orderBy('id', 'DESC')->get();
+
+        return response()->json([
+            'products' => $products,
+        ], 200);
+    }
 }
