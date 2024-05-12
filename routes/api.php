5ee8fa3671a784e4c6bb2be68d499b03e37322e9
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-13 01:42:59 +0600

    feat(api-routes): Add endpoint for retrieving all customers

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\InvoiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,3 +24,5 @@ Route::get('/get_all_invoice', [InvoiceController::class, 'get_all_invoice']);
 Route::get('/search_invoice', [InvoiceController::class, 'search_invoice']);
 Route::get('/create_invoice', [InvoiceController::class, 'create_invoice']);
 Route::post('/add_invoice', [InvoiceController::class, 'add_invoice']);
+
+Route::get('/customers', [CustomerController::class, 'all_customer']);
