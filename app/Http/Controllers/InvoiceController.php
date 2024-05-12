18f2fe65046e9200e40f8343f13fb351641b4e16
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-13 01:31:56 +0600

    feat(controllers): :sparkles: add functionality to create invoices and invoice items

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Counter;
 use App\Models\Invoice;
+use App\Models\InvoiceItem;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
@@ -67,5 +68,31 @@ class InvoiceController extends Controller
         ];
 
         return response()->json($formData);
+    }
+
+    public function add_invoice(Request $request)
+    {
+        $invoiceItem = $request->input("invoice_item");
+
+        $invoiceData['sub_total'] = $request->input("sub_total");
+        $invoiceData['total'] = $request->input("total");
+        $invoiceData['customer_id'] = $request->input("customer_id");
+        $invoiceData['number'] = $request->input("number");
+        $invoiceData['date'] = $request->input("date");
+        $invoiceData['due_date'] = $request->input("due_date");
+        $invoiceData['discount'] = $request->input("discount");
+        $invoiceData['reference'] = $request->input("reference");
+        $invoiceData['terms'] = $request->input("terms");
+
+        $invoice = Invoice::create($invoiceData);
+
+        foreach (json_decode($invoiceItem) as $item) {
+            $itemData['product_id'] = $item->id;
+            $itemData['invoice_id'] = $invoice->id;
+            $itemData['quantity'] = $item->quantity;
+            $itemData['unit_price'] = $item->unit_price;
+
+            InvoiceItem::create($itemData);
+        }
     }
 }
