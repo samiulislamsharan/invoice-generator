d6a02a2db23eedbee2d36038d2027447c614f3c0
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-14 01:16:21 +0600

    feat(router): add invoice edit route and component

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router"
 import invoiceIndex from '../components/invoices/index.vue';
 import invoiceNew from '../components/invoices/new.vue';
 import invoiceShow from '../components/invoices/show.vue';
+import invoiceEdit from '../components/invoices/edit.vue';
 import notFound from '../components/NotFound.vue';
 
 const routes = [
@@ -17,6 +18,11 @@ const routes = [
     {
         path: '/invoice/show/:id',
         component: invoiceShow,
+        props: true,
+    },
+    {
+        path: '/invoice/edit/:id',
+        component: invoiceEdit,
         props: true,
     },
     {
