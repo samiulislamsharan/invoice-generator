e8a5ccf91d570d2744834ca90de74bfa8538ad37
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-13 11:39:18 +0600

    feat(router): :sparkles: add showInvoice component and endpoint for displaying invoice details

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router"
 
 import invoiceIndex from '../components/invoices/index.vue';
 import invoiceNew from '../components/invoices/new.vue';
+import invoiceShow from '../components/invoices/show.vue';
 import notFound from '../components/NotFound.vue';
 
 const routes = [
@@ -12,6 +13,11 @@ const routes = [
     {
         path: '/invoice/new',
         component: invoiceNew,
+    },
+    {
+        path: '/invoice/show/:id',
+        component: invoiceShow,
+        props: true,
     },
     {
         path: '/:pathMatch(.*)*',
