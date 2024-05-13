1a80b78e27093caa3bc5f4063f1902a00b0e4abe
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-13 11:33:44 +0600

    feat(models): :sparkles: add invoice_items relationship to Invoice model
    This commit adds the `invoice_items` relationship to the `Invoice` model, allowing it to have multiple invoice items associated with it. This enhancement improves the functionality of the application by providing a way to retrieve all invoice items for a specific invoice.

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -25,4 +25,9 @@ class Invoice extends Model
     {
         return $this->belongsTo(Customer::class);
     }
+
+    public function invoice_items()
+    {
+        return $this->hasMany(InvoiceItem::class);
+    }
 }
