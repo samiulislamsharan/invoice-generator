ad449fb09a5268810120a57dd90797d91e22a1f7
Samiul Islam <samiulislamsharan@gmail.com>
2024-05-14 01:16:47 +0600

    feat(api-routes): add routes for invoice editing and deletion
    This commit adds new routes to the API for editing and deleting invoices. It includes the following changes:
    
    - Added `/edit_invoice/{id}` route to edit an existing invoice.
    - Added `/delete_invoice_items/{id}` route to delete items from an invoice.
    - Added `/update_invoice/{id}` route to update an existing invoice.
    - Added `/delete_invoice/{id}` route to delete an existing invoice.
    
    These routes enhance the functionality of the API by allowing users to edit and delete invoices.

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,10 @@ Route::get('/search_invoice', [InvoiceController::class, 'search_invoice']);
 Route::get('/create_invoice', [InvoiceController::class, 'create_invoice']);
 Route::post('/add_invoice', [InvoiceController::class, 'add_invoice']);
 Route::get('/show_invoice/{id}', [InvoiceController::class, 'show_invoice']);
+Route::get('/edit_invoice/{id}', [InvoiceController::class, 'edit_invoice']);
+Route::get('/delete_invoice_items/{id}', [InvoiceController::class, 'delete_invoice_items']);
+Route::post('/update_invoice/{id}', [InvoiceController::class, 'update_invoice']);
+Route::get('/delete_invoice/{id}', [InvoiceController::class, 'delete_invoice']);
 
 Route::get('/customers', [CustomerController::class, 'all_customer']);
 
